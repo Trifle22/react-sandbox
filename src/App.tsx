859ed936bbb4294f71680666cb637bba98e57373
Trifle22 <xromchenkov99@gmail.com>
2023-02-11 19:15:08 +0300

    init

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './app.css'
+function App() {
+  return (
+    <div className='root'>
+      <p className='paragraph'>This is the react sandbox, enjoy!</p>
+    </div>
+  );
+}
+
+export default App;
